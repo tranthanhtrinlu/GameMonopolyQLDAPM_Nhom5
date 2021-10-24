ec0993615263370a59e5fc18d952fc04ada91c78
maxcurkovic <91294718+maxcurkovic@users.noreply.github.com>
2021-10-24 13:13:49 -0400

    Update PropertyListener.java

--- src/PropertyListener.java
+++ src/PropertyListener.java
@@ -1,3 +1,6 @@
+/**
+ * Interface PropertyListener that describes the listener for a Property element.
+ */
 public interface PropertyListener {
     void propertyNoOwner(PropertyEvent e);
     void propertyOwned(PropertyEvent e);
