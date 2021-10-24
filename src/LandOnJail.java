65d77de43a63970a1c596fa489d96fbd49b3df98
coryhelm <cory_17h@hotmail.com>
2021-10-24 17:18:19 -0400

    landonjail java doc

--- src/LandOnJail.java
+++ src/LandOnJail.java
@@ -4,11 +4,23 @@ import java.util.List;
 public class LandOnJail extends Location {
 
     private List<LandOnJailListener> landOnJailListenerList;
+
+    /**
+     * constructor for LandOnJail
+     * @param cost Integer cost to get out of jail
+     * @param name String
+     */
     public LandOnJail(int cost, String name) {
         super(cost, name);
         this.landOnJailListenerList = new ArrayList<>();
     }
 
+    /**
+     *  location functionality
+     * @param p Player
+     * @param totalDiceRoll Integer sum of dice roll
+     * @return land on jail listener
+     */
     @Override
     public boolean locationElementFunctionality(Player p, int totalDiceRoll) {
         for (LandOnJailListener listener : this.landOnJailListenerList){
@@ -17,26 +29,48 @@ public class LandOnJail extends Location {
         return false;
     }
 
+    /**
+     * send info to a string
+     * @param p Player
+     * @return player name and landed on just visiting
+     */
     @Override
     public String toString(Player p) {
         return p.getPlayerName() + " LANDED ON JAIL. Just Visiting";
     }
 
+    /**
+     * the play can not buy jail
+     * @param p Player
+     * @return false
+     */
     @Override
     public boolean buy(Player p) {
         return false;
     }
 
+    /**
+     * reset the owner
+     */
     @Override
     public void resetOwner() {
 
     }
 
+    /**
+     * gets the result fo board event
+     * @param p Player
+     * @param event BoardEven
+     */
     @Override
     public void getResult(Player p,  BoardEvent event) {
 
     }
 
+    /**
+     * adds the listener for jail
+     * @param view
+     */
     @Override
     public void addListener(BoardView view) {
         this.landOnJailListenerList.add(view);
