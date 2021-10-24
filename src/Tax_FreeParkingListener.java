74edaf2de07f16c7a629e4093126a228404ed855
maxcurkovic <91294718+maxcurkovic@users.noreply.github.com>
2021-10-24 13:14:15 -0400

    Update Tax_FreeParkingListener.java

--- src/Tax_FreeParkingListener.java
+++ src/Tax_FreeParkingListener.java
@@ -1,3 +1,6 @@
+/**
+ * Interface Tax_FreeParkingListener that describes the listener for a Free Parking or Tax element.
+ */
 public interface Tax_FreeParkingListener {
     void FreeParking(Tax_FreeParkingEvent e);
     void payTax(Tax_FreeParkingEvent e);
