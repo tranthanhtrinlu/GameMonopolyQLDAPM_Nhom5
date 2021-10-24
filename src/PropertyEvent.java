d3a1d3495d1072e802e31590d75f41ee3018954b
coryhelm <cory_17h@hotmail.com>
2021-10-24 16:44:53 -0400

    property even java doc

--- src/PropertyEvent.java
+++ src/PropertyEvent.java
@@ -4,6 +4,11 @@ public class PropertyEvent extends EventObject {
 
     private Player player;
 
+    /**
+     * constructor for property event
+     * @param property Property
+     * @param p Player
+     */
     public PropertyEvent(Property property, Player p) {
         super(property);
         this.player = p;
