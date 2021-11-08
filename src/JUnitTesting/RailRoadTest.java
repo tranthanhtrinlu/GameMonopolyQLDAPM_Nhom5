2951bfb52722edc3854db798d799285d28811271
Max Curkovic <deekey45@gmail.com>
2021-11-08 09:17:32 -0500

    Almost done testing

--- src/JUnitTesting/RailRoadTest.java
+++ src/JUnitTesting/RailRoadTest.java
@@ -1,48 +1,44 @@
 package JUnitTesting;
 
+import Model.Player;
+import Model.RailRoad;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * @author Max Curkovic
+ * All necessary tests for the RailRoad class.
+ */
 public class RailRoadTest {
 
+
+    /**
+     * Tests the buy() method.
+     */
     @Test
-    public void getPayment() {
+    public void testBuy() {
+
+        RailRoad railRoad = new RailRoad("Short Line", 100);
+        Player player = new Player("A");
+        player.setMoneyAmount(100);
+        railRoad.buy(player);
+        assertEquals(1, player.getNumOfProperties());
+
     }
 
+    /**
+     * Tests the resetOwner() method.
+     */
     @Test
-    public void testGetPayment() {
+    public void testResetOwner() {
+        RailRoad railRoad = new RailRoad("Short Line", 100);
+        Player player = new Player("A");
+        player.setMoneyAmount(100);
+        railRoad.buy(player);
+        railRoad.resetOwner();
+        assertNull(railRoad.getOwner());
     }
 
-    @Test
-    public void getOwner() {
-    }
 
-    @Test
-    public void locationElementFunctionality() {
-    }
-
-    @Test
-    public void buy() {
-    }
-
-    @Test
-    public void resetOwner() {
-    }
-
-    @Test
-    public void testGetPayment1() {
-    }
-
-    @Test
-    public void addListener() {
-    }
-
-    @Test
-    public void testGetPayment2() {
-    }
-
-    @Test
-    public void testGetOwner() {
-    }
 }
--- src/JUnitTesting/UtilityTest.java
+++ src/JUnitTesting/UtilityTest.java
@@ -1,32 +1,47 @@
 package JUnitTesting;
 
+import Model.Player;
+import Model.Utility;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class UtilityTest {
 
+    /**
+     * Tests the buy() method.
+     */
     @Test
-    public void buy() {
+    public void testBuy() {
+
+        Utility utility = new Utility("Electric Company", 100);
+        Player player = new Player("A");
+        player.setMoneyAmount(100);
+        utility.buy(player);
+        assertEquals(1, player.getNumOfProperties());
+
+    }
+
+    /**
+     * Tests the resetOwner() method.
+     */
+    @Test
+    public void testResetOwner() {
+        Utility utility = new Utility("Electric Company", 100);
+        Player player = new Player("A");
+        player.setMoneyAmount(100);
+        utility.buy(player);
+        utility.resetOwner();
+        assertNull(utility.getOwner());
     }
 
     @Test
-    public void resetOwner() {
-    }
+    public void testPayment() {
+        Utility utility = new Utility("Electric Company", 100);
+        Player player1 = new Player("A");
+        player1.setMoneyAmount(100);
+        utility.buy(player1);
+        assertEquals(28, utility.payment(7));
 
-    @Test
-    public void addListener() {
-    }
-
-    @Test
-    public void getOwner() {
-    }
-
-    @Test
-    public void locationElementFunctionality() {
-    }
-
-    @Test
-    public void payment() {
     }
 }
