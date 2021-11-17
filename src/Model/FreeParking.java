a3c479b8d6f5cfd7c340aac1454519f378dbdb21
Max Curkovic <deekey45@gmail.com>
2021-11-17 10:30:38 -0500

    Separated Tax and Free Parking

--- src/Model/FreeParking.java
+++ src/Model/FreeParking.java
@@ -6,17 +6,33 @@ import Listener.FreeParkingListener;
 
 import java.util.ArrayList;
 
+/**
+ * @author Max Curkovic
+ * Class MVC.FreeParking for describing a Free Parking element.
+ */
 public class FreeParking extends Location{
 
 
     ArrayList<FreeParkingListener> listeners;
 
+    /**
+     * Default constructor for MVC.FreeParking.
+     * @param cost An integer cost.
+     * @param name A String name.
+     */
     public FreeParking(int cost, String name){
         super(cost, name);
         this.listeners = new ArrayList<>();
 
     }
 
+    /**
+     * Boolean method for listening to the Free Pass element.
+     * @param p A MVC.Player object p.
+     * @param totalDiceRoll An integer totalDiceRoll.
+     * @param currentTurn An integer currentTurn.
+     * @return Will always return false.
+     */
     @Override
     public boolean locationElementFunctionality(Player p, int totalDiceRoll, int currentTurn) {
         if (BoardModel.centerMoney == 0)
@@ -29,21 +45,36 @@ public class FreeParking extends Location{
         return false;
     }
 
+    /**
+     * Overriden Java method toString for Free Parking.
+     * @param p A MVC.Player object p.
+     * @return A String.
+     */
     @Override
     public String toString(Player p) {
         return p.getPlayerName() + " Landed on free parking. Earned money is $";
     }
 
+    /**
+     * Does nothing for this class.
+     */
     @Override
     public boolean buy(Player p) {
         return false;
     }
 
+    /**
+     * Does nothing for this class.
+     */
     @Override
     public void resetOwner() {
 
     }
 
+    /**
+     * Adds the view to the ArrayList of Free Parking listeners
+     * @param view A Listener.BoardView view
+     */
     @Override
     public void addListener(BoardView view) {
         this.listeners.add(view);
