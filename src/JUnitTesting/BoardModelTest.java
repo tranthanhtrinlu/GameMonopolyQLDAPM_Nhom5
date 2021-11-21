c55601c9443f9e956bf16c01a2173af510850268
Max Curkovic <deekey45@gmail.com>
2021-11-21 10:32:04 -0500

    Fixing stuff, including testing

---
+++ src/JUnitTesting/BoardModelTest.java
@@ -0,0 +1,15 @@
+package JUnitTesting;
+
+import Model.BoardModel;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Max Curkovic
+ * All necessary tests for the BoardModel class.
+ */
+public class BoardModelTest {
+
+
+}
